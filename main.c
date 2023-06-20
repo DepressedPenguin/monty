c24e04fd03936d0fcfe19d7cc9b327ec18353409
ZAKARIA <zakariaelaro@gmail.com>
2023-06-20 14:33:09 -0700

    check if work without betty

--- main.c
+++ main.c
@@ -2,75 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 #include "monty.h"
-
 #define MAX_LINE_LENGTH 100
-
+/**
+* process_file - Process the commands
+* @filename: Name file
+*/
 void process_file(const char *filename)
 {
-    FILE *file;
-    char line[MAX_LINE_LENGTH];
-    int line_number = 1;
-    char *opcode, *arg;
-    stack_tm *stack = NULL;
-
-    file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    while (fgets(line, sizeof(line), file))
-    {
-        line[strcspn(line, "\n")] = '\0';
-
-        opcode = strtok(line, " ");
-        if (opcode != NULL)
-        {
-            arg = strtok(NULL, " ");
-            if (strcmp(opcode, "push") == 0)
-            {
-                if (arg != NULL)
-                {
-                    int value = atoi(arg);
-                    push(&stack, value);
-                }
-                else
-                {
-                    fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
-                    exit(EXIT_FAILURE);
-                }
-            }
-            else if (strcmp(opcode, "pall") == 0)
-            {
-                pall(&stack);
-            }
-            else
-            {
-                fprintf(stderr, "Error: L%d: unknown instruction %s\n", line_number, opcode);
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        line_number++;
-    }
-
-    fclose(file);
+FILE *file;
+char line[MAX_LINE_LENGTH];
+int line_number = 1;
+char *opcode;
+char *arg;
+stack_tm *stack = NULL;
+file = fopen(filename, "r");
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+exit(EXIT_FAILURE);
 }
-
+while (fgets(line, sizeof(line), file))
+{
+line[strcspn(line, "\n")] = '\0';
+opcode = strtok(line, " ");
+arg = strtok(NULL, " ");
+if (opcode != NULL)
+{
+if (strcmp(opcode, "push") == 0)
+{
+if (arg != NULL)
+{
+    int value = atoi(arg);
+    push(&stack, value);
+}
+else
+{
+    fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
+    exit(EXIT_FAILURE);
+	}
+    }
+    else if (strcmp(opcode, "pall") == 0)
+    {
+	pall(&stack);
+    }
+    else
+    {
+	fprintf(stderr, "Error: L%d: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
+    }
+}
+line_number++;
+}
+fclose(file);
+}
+/**
+* main - Entry point
+* @argc: Number of cmd args
+* @argv: Array of cmds.
+* Return: 0 on success 1
+*/
 int main(int argc, char *argv[])
 {
-    const char *filename;
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    filename = argv[1];
-    process_file(filename);
-
-    return 0;
+const char *filename;
+if (argc != 2)
+{
+fprintf(stderr, "Usage: monty file\n");
+exit(EXIT_FAILURE);
+}
+filename = argv[1];
+process_file(filename);
+return 0;
 }
 
